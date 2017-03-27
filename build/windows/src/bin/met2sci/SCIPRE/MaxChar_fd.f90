4c05af7d03a9658ea8414c19faca51609dc89382
Sean Vail <svail@epri.com>
2017-03-27 09:42:05 -0700

    taking down 3.0

--- build/windows/src/bin/met2sci/SCIPRE/MaxChar_fd.f90
+++
@@ -1,5 +0,0 @@
-MODULE MaxChar_fd
-
-  INTEGER, PARAMETER :: MAXC = 512  !Max path+file name length
-
-END MODULE MaxChar_fd
--- src/dll/SCIPUFF/SWIM/SWIMread3DClimo.f90
+++
@@ -1,9 +0,0 @@
-!*******************************************************************************
-!$RCSfile$
-!$Revision$
-!$Date$
-!*******************************************************************************
-INTEGER FUNCTION SWIMread3DClimo( TimePrj,TimeFld,Src,TimeOut,MeanFld,VarFld,BLFld )
-
-RETURN
-END
